38f57b24b9aaf7995b6efac81950f328ccc9d0aa
Adrian Puertas Cabedo <al259348@uji.es>
2015-11-17 14:36:22 +0000

    Home controller fixed and WebSecurityConfig

--- src/main/java/ie/cit/adf/muss/controller/HomeController.java
+++ src/main/java/ie/cit/adf/muss/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	@RequestMapping("/")
+	@RequestMapping(value={"/", "/home"})
 	public String home() {
 		return "home";
 	}
-
+	
 }
