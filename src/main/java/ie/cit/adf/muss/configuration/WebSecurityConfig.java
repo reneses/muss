822f7171c898b4b9e505e88c0b64b77d3458715c
DavidFloydd <d.macias-rodriguez@mycit.ie>
2015-12-02 20:51:56 +0000

    Security updated for static resources

--- src/main/java/ie/cit/adf/muss/configuration/WebSecurityConfig.java
+++ src/main/java/ie/cit/adf/muss/configuration/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter  {
     protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests()
-            .antMatchers("/", "/home", "/register", "/console/**", "/gallery/**").permitAll()
+            .antMatchers("/", "/home", "/register", "/console/**", "/gallery/**", "/css/**", "/webjars/**").permitAll()
             .anyRequest().authenticated()
             .and()
         .formLogin()
