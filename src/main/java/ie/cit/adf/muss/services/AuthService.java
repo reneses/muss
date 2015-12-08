dcf0f9cd3cfbb6b122ac40386d8e6593e92f9bda
Álvaro Reneses <alvaroreneses@gmail.com>
2015-12-08 18:55:59 +0000

    AuthService refactoring + getPrincipal() returns null if there is no user logged (instead of non-catched exception)

--- src/main/java/ie/cit/adf/muss/services/AuthService.java
+++ src/main/java/ie/cit/adf/muss/services/AuthService.java
@@ -1,94 +1,61 @@
 package ie.cit.adf.muss.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 import ie.cit.adf.muss.domain.User;
 import ie.cit.adf.muss.exceptions.EmailExistsException;
 import ie.cit.adf.muss.exceptions.UsernameExistsException;
 import ie.cit.adf.muss.validation.UserForm;
 
-/**
- * TODO: add profile picture TODO: work with hashed passwords
- */
 @Service
 public class AuthService {
 
-	@Autowired
-	UserService userService;
+    @Autowired
+    UserService userService;
 
-	@Transactional
-	public User register(UserForm userForm) throws UsernameExistsException, EmailExistsException {
+    @Transactional
+    public User register(UserForm userForm) throws UsernameExistsException, EmailExistsException {
 
-		// Check that there is no user registered with the same username / email
-		if (userService.usernameExists(userForm.getUsername()))
-			throw new UsernameExistsException(
-					"There already exists an account with the username: " + userForm.getUsername());
-		if (userService.emailExists(userForm.getEmail()))
-			throw new EmailExistsException("There already exists an account with the email: " + userForm.getEmail());
+        // Check that there is no user registered with the same username / email
+        if (userService.usernameExists(userForm.getUsername()))
+            throw new UsernameExistsException(
+                    "There already exists an account with the username: " + userForm.getUsername());
+        if (userService.emailExists(userForm.getEmail()))
+            throw new EmailExistsException("There already exists an account with the email: " + userForm.getEmail());
 
-		// Create and store the user
-		User user = new User();
-		user.setName(userForm.getName());
-		user.setUsername(userForm.getUsername());
-		user.setEmail(userForm.getEmail());
-		user.setPassword(userForm.getPassword());
-		return userService.save(user);
+        // Create and store the user
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setUsername(userForm.getUsername());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        return userService.save(user);
 
-	}
+    }
 
-	// Managed repository -----------------------------------------------------
+    // Managed repository -----------------------------------------------------
 
-	// Business methods -------------------------------------------------------
+    // Business methods -------------------------------------------------------
 
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// Assert.notNull(username);
-		//
-		// UserDetails result;
-		//
-		// result = userRepository.findByUsername(username);
-		// Assert.notNull(result);
-		// // WARNING: The following sentences prevent lazy initialisation
-		// problems!
-		// Assert.notNull(result.getAuthorities());
-		// result.getAuthorities().size();
+    public User getPrincipal() {
 
-		return null;
-	}
+        // Obtain the principal
+        Object principal = SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
 
-	public User getPrincipal() {
-		User result;
-		SecurityContext context;
-		Authentication authentication;
-		Object principal;
+        // If the user is not logged, return null
+        if (principal.equals("anonymousUser"))
+            return null;
 
-		// If the asserts in this method fail, then you're
-		// likely to have your Tomcat's working directory
-		// corrupt. Please, clear your browser's cache, stop
-		// Tomcat, update your Maven's project configuration,
-		// clean your project, clean Tomcat's working directory,
-		// republish your project, and start it over.
-
-		context = SecurityContextHolder.getContext();
-		Assert.notNull(context);
-		authentication = context.getAuthentication();
-		Assert.notNull(authentication);
-		principal = authentication.getPrincipal();
-//		Assert.isTrue(principal instanceof User);
-		org.springframework.security.core.userdetails.User userDB =
-								(org.springframework.security.core.userdetails.User) principal;
-		result = userService.findByUsername(userDB.getUsername());
-		Assert.notNull(result);
-		Assert.isTrue(result.getId() != 0);
-
-		return result;
-	}
+        // If it is, return find it and return it
+        org.springframework.security.core.userdetails.User userDB =
+                (org.springframework.security.core.userdetails.User) principal;
+        return userService.findByUsername(userDB.getUsername());
+    }
 
 }
