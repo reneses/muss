5a8f84a29e35387e05ccdbfe41798da189605da1
Adrian Puertas Cabedo <al259348@uji.es>
2015-12-08 11:52:16 +0000

    object method in gallery controller cleaned

--- src/main/java/ie/cit/adf/muss/controller/GalleryController.java
+++ src/main/java/ie/cit/adf/muss/controller/GalleryController.java
@@ -27,56 +27,6 @@ public class GalleryController {
 	@RequestMapping(value="/gallery/{objectID}", method = RequestMethod.GET)
 	public String object(Model model, @PathVariable int objectID) {
 
-		/*ChObject object = new ChObject();
-		object.setTitle("TITLE");
-		object.setCreditLine("LINE");
-		object.setDate("DATE");
-		object.setDescription("DESCRIPTION");
-		object.setGalleryText("GALLERY");
-		object.setMedium("MEDIUM");
-		object.setOriginalId(1001);
-
-		ImageSize size = new ImageSize();
-		size.setHeight(100);
-		size.setWidth(200);
-		size.setUrl("http://www.jpl.nasa.gov/spaceimages/images/mediumsize/PIA17011_ip.jpg");
-		size.setLabel("s");
-		List<ImageSize> sizes = new ArrayList<>();
-		sizes.add(size);
-
-		Image image = new Image();
-		image.setOriginalId(3001);
-		image.setPrimary(true);
-		image.setSizes(sizes);
-
-		ImageSize size2 = new ImageSize();
-		size2.setHeight(100);
-		size2.setWidth(200);
-		size2.setUrl("http://www.nycgovparks.org/photo_gallery/full_size/14413.jpg");
-		size2.setLabel("s");
-		List<ImageSize> sizes2 = new ArrayList<>();
-		sizes2.add(size2);
-
-		Image image2 = new Image();
-		image2.setOriginalId(3001);
-		image2.setPrimary(true);
-		image2.setSizes(sizes2);
-
-
-		List<Image> images = new ArrayList<>();
-		images.add(image);
-		images.add(image2);
-		object.setImages(images);
-
-		List<Tag> tags = new ArrayList<>();
-		Tag t = new Tag();
-		t.setName("PICTURE");
-		tags.add(t);
-		t = new Tag();
-		t.setName("NEO IMPRESSIONISM");
-		tags.add(t);
-		object.setTags(tags);*/
-
 		ChObject object = objectService.find(objectID);
 
 		model.addAttribute("object", object);
