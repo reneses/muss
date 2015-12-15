66b57b3f8e9180a9041bf2bead86138ab780e303
Adrian Puertas Cabedo <al259348@uji.es>
2015-12-15 22:38:19 +0100

    Edit Profile Form

--- src/main/java/ie/cit/adf/muss/controller/UserController.java
+++ src/main/java/ie/cit/adf/muss/controller/UserController.java
@@ -3,9 +3,13 @@ package ie.cit.adf.muss.controller;
 import java.util.Date;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +18,7 @@ import ie.cit.adf.muss.domain.User;
 import ie.cit.adf.muss.services.APIService;
 import ie.cit.adf.muss.services.AuthService;
 import ie.cit.adf.muss.services.UserService;
+import ie.cit.adf.muss.validation.EditProfileForm;
 
 @Controller
 @RequestMapping("/user")
@@ -59,6 +64,71 @@ public class UserController {
         model.addAttribute("HMAC", user == null? "" : apiService.getHMAC(user, date));
 		
 		return "user/profile";
+	}
+	
+	//	Edit ------------------------------------------------------------------
+	
+	@RequestMapping(value="/editProfile", method=RequestMethod.GET)
+	public String editProfile(Model model) {
+		
+		User user = authService.getPrincipal();
+		
+		EditProfileForm form = new EditProfileForm();
+		form.setName(user.getName());
+		form.setEmail(user.getEmail());
+		form.setUsername(user.getUsername());
+		
+		model.addAttribute("form", form);
+		
+		return "user/editProfile";
+	}
+	
+	@RequestMapping(value="/editProfile", method=RequestMethod.POST)
+	public String editProfilePost(@Valid @ModelAttribute("form") EditProfileForm form, BindingResult bindingResult) {
+		
+		User user = authService.getPrincipal();
+		
+		if (!form.getUsername().equals(user.getUsername()) && 
+				userService.usernameExists(form.getUsername())) {
+			bindingResult.rejectValue("username", "UsernameNotAvailable", "Username not available");	
+		}
+
+		if (!form.getEmail().equals(user.getEmail()) && 
+				userService.emailExists(form.getEmail())) {
+			bindingResult.rejectValue("email", "EmailNotAvailable", "Email not available");	
+		}
+		
+		if (bindingResult.hasErrors()) {
+            return "user/editProfile";
+        }
+
+		user.setName(form.getName());
+		user.setEmail(form.getEmail());
+		user.setUsername(form.getUsername());
+		
+		user = userService.save(user);
+        
+        return "redirect:/user/profile";
+	}
+	
+	@RequestMapping(value="/changePicture", method=RequestMethod.GET)
+	public String changePicture(Model model) {
+		return "user/changePicture";
+	}
+	
+	@RequestMapping(value="/changePicture", method=RequestMethod.POST)
+	public String changePicturePost() {
+		return "";
+	}
+	
+	@RequestMapping(value="/changePassword", method=RequestMethod.GET)
+	public String changePassword(Model model) {
+		return "user/changePassword";
+	}
+	
+	@RequestMapping(value="/changePassword", method=RequestMethod.POST)
+	public String changePasswordPost() {
+		return "";
 	}
 
 	//	Follow & Unfollow -----------------------------------------------------
